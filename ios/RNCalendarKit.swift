fe19466e382013ed8a4b0bcccb0b336bb51a357d
Eric Svensson <ericsvensson@Erics-MacBook-Pro.local>
2018-10-29 18:10:46 +0100

    Added placeholder

---
+++ ios/RNCalendarKit.swift
@@ -0,0 +1,13 @@
+import Foundation
+import CalendarKit
+import DateToolsSwift
+
+
+class HeaderController: UIViewController, DayViewStateUpdating {
+    func move(from oldDate: Date, to newDate: Date) {
+        
+    }
+    
+    
+}
+
